8b0a5645052a016b15801a749a2660b46fed2a34
Jennifer Chen <jennifchen@nvidia.com>
2026-02-18 19:45:17 +0000

    shared_moe_weight_scale parameter
    Signed-off-by: Jennifer Chen <jennifchen@nvidia.com>

--- tests/gpu_megatron/torch/quantization/plugins/test_megatron.py
+++ tests/gpu_megatron/torch/quantization/plugins/test_megatron.py
@@ -737,7 +737,8 @@ def test_te_grouped_vs_sequential_quantize(need_4_gpus):
 
 @pytest.mark.parametrize("ep_size", [1, 2])
 @pytest.mark.parametrize("moe_grouped_gemm", [True, False])
-def test_layer_sync_moe_local_experts_amax(ep_size, moe_grouped_gemm):
+@pytest.mark.parametrize("shared_moe_weight_scale", [True, False])
+def test_layer_sync_moe_local_experts_amax(ep_size, moe_grouped_gemm, shared_moe_weight_scale):
     """Test expert model parallel synchronization."""
     size = torch.cuda.device_count()
     if size < ep_size:
@@ -749,12 +750,15 @@ def test_layer_sync_moe_local_experts_amax(ep_size, moe_grouped_gemm):
             _test_layer_sync_moe_local_experts_amax,
             ep_size,
             moe_grouped_gemm,
+            shared_moe_weight_scale,
         ),
         backend="nccl",
     )
 
 
-def _test_layer_sync_moe_local_experts_amax(ep_size, moe_grouped_gemm, rank, size):
+def _test_layer_sync_moe_local_experts_amax(
+    ep_size, moe_grouped_gemm, shared_moe_weight_scale, rank, size
+):
     initialize_for_megatron(
         tensor_model_parallel_size=1,
         pipeline_model_parallel_size=1,
@@ -776,7 +780,7 @@ def _test_layer_sync_moe_local_experts_amax(ep_size, moe_grouped_gemm, rank, siz
 
     # Sync amax across local experts in each layer
     for layer in model.decoder.layers:
-        layer.mlp.experts.layer_sync_moe_local_experts_amax()
+        layer.mlp.experts.layer_sync_moe_local_experts_amax(shared_moe_weight_scale)
 
     for layer in model.decoder.layers:
         fc1_amax = None
@@ -792,6 +796,22 @@ def _test_layer_sync_moe_local_experts_amax(ep_size, moe_grouped_gemm, rank, siz
                 fc2_amax = expert.linear_fc2.input_quantizer.amax
             else:
                 assert torch.allclose(fc2_amax, expert.linear_fc2.input_quantizer.amax)
+
+    if shared_moe_weight_scale:
+        for layer in model.decoder.layers:
+            fc1_amax = None
+            fc2_amax = None
+            for expert in layer.mlp.experts.local_experts:
+                assert expert.linear_fc1.weight_quantizer.amax is not None
+                assert expert.linear_fc2.weight_quantizer.amax is not None
+                if fc1_amax is None:
+                    fc1_amax = expert.linear_fc1.weight_quantizer.amax
+                else:
+                    assert torch.allclose(fc1_amax, expert.linear_fc1.weight_quantizer.amax)
+                if fc2_amax is None:
+                    fc2_amax = expert.linear_fc2.weight_quantizer.amax
+                else:
+                    assert torch.allclose(fc2_amax, expert.linear_fc2.weight_quantizer.amax)
 
 
 def _test_expert_model_parallel_amax_sync(
